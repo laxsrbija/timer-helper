a6c5cf0f52022daa7dacf7a22eac38eafeef32a4
Lazar Stanojević <me@lazars.net>
2023-03-29 18:55:04 +0200

    Migrate existing Python app to Java

---
+++ src/main/java/net/lazars/timer/TimerHelperApplication.java
@@ -0,0 +1,12 @@
+package net.lazars.timer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimerHelperApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TimerHelperApplication.class, args);
+  }
+}
---
+++ src/main/java/net/lazars/timer/TimerRestController.java
@@ -0,0 +1,57 @@
+package net.lazars.timer;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.TimeZone;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TimerRestController {
+
+  private static final String TIME_FORMAT_PATTERN = "H:mm";
+  private static final DateTimeFormatter TIME_FORMAT =
+      DateTimeFormatter.ofPattern(TIME_FORMAT_PATTERN);
+  private static final DateTimeFormatter DATE_TIME_FORMAT =
+      DateTimeFormatter.ofPattern("HH:mm dd-MM-yyyy");
+
+  @Value("${target-base-time}")
+  private String targetBaseTime;
+
+  @GetMapping("target")
+  public String getTargetBaseTime(final TimeZone timeZone) {
+    return getTargetTime(timeZone).format(TIME_FORMAT);
+  }
+
+  @GetMapping("calculate")
+  public CalculationResponse calculateFinishTime(
+      @RequestParam("runtime") @DateTimeFormat(pattern = TIME_FORMAT_PATTERN)
+          final LocalTime runtime,
+      final TimeZone timeZone) {
+    final LocalTime targetTime = getTargetTime(timeZone);
+    final LocalDateTime finishTime =
+        LocalDateTime.of(
+            targetTime.getHour() > 6 ? LocalDate.now().plusDays(1) : LocalDate.now(), targetTime);
+
+    final LocalDateTime startTime =
+        finishTime.minusHours(runtime.getHour()).minusMinutes(runtime.getMinute());
+
+    return new CalculationResponse(
+        startTime.format(DATE_TIME_FORMAT), finishTime.format(TIME_FORMAT));
+  }
+
+  private LocalTime getTargetTime(final TimeZone timeZone) {
+    final boolean isDst = timeZone.inDaylightTime(new Date());
+    return LocalTime.parse(targetBaseTime).plusHours(isDst ? 1 : 0);
+  }
+
+  private record CalculationResponse(String startTime, String timeToStart) {}
+}
